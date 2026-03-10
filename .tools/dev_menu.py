5d3c8a8b1a148ea832f26f4d0780842a9bf75f9b
Anton Stechman <stechman.ab@gmail.com>
2026-03-10 18:42:08 +1300

    PATCH: dev tools setup (#2)
    # Pull Request Template
    
    ## Description
    
    Setup Dev tools menu
    
    ## Type of Change
    
    Please delete options that are not relevant.
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update
    - [ ] Refactor
    - [ ] Test coverage improvement
    
    ## Checklist
    
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [x] I have made corresponding changes to the documentation
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [x] New and existing unit tests pass locally with my changes
    - [x] Any dependent changes have been merged and published in downstream
    modules
    
    ## How Has This Been Tested?
    
    N/A
    
    - [ ] Test A
    - [ ] Test B
    
    ## Screenshots (if applicable)
    
    ## Related Issues
    
    ## Additional Notes
    
    Add any other context about the pull request here.

---
+++ .tools/dev_menu.py
@@ -0,0 +1,76 @@
+import os
+
+def setup_project():
+    os.system("powershell -ExecutionPolicy Bypass -File .tools/setup.ps1")
+
+def run_pytest():
+    os.system("pytest")
+
+def run_pylint():
+    os.system("pylint src/")
+
+def build_package():
+    os.system("python -m build ./src")
+
+MENU_ITEMS: list[dict] = [
+    {"name": "Run Setup", "action": setup_project}
+    , {"name": "Run PyLint", "action": run_pylint}
+    , {"name": "Run PyTest", "action": run_pytest}
+    , {"name": "Build Package", "action": build_package}
+] if os.path.isdir(".venv") else [{"name": "Run Setup", "action": setup_project}]
+
+# If project has not been initialised yet - force user to initialise
+ACCESSABLE_MENU_ITEMS: list[dict] = (
+    MENU_ITEMS if os.path.isdir(".venv")
+    else [MENU_ITEMS[0]]
+)
+
+def run_option(index: int):
+    if index < 0 or index > len(ACCESSABLE_MENU_ITEMS) - 1:
+        raise IndexError(index)
+    item = ACCESSABLE_MENU_ITEMS[index]
+
+    if not isinstance(item, dict):
+        raise ValueError(item)
+
+    action = item["action"]
+    action()
+    input("Complete! press Enter to continue")
+
+def print_items():
+    for i, item in enumerate(ACCESSABLE_MENU_ITEMS):
+        i = i + 1
+        if not isinstance(item, dict):
+            continue
+        name: str = item["name"]
+        print(f"- [{i}] {name}")
+
+def main():
+    while True:
+        os.system("cls")
+        print("=" * 40)
+        print("Snowstream CLI Dev Menu")
+        print("=" * 40)
+        print_items()
+        print("- [0] exit")
+        resonse = input("\nSelect an option : ").strip()
+        try:
+            resonse = int(resonse)
+
+            if resonse == 0:
+                print("Exiting the menu...")
+                break
+            else:
+                run_option(resonse - 1)
+        except (ValueError, IndexError, KeyError, TypeError):
+            print(f"Selection '{resonse}' is invalid")
+            input("press enter to continue")
+
+            # Re-initialize menu if .venv now exists
+            global ACCESSABLE_MENU_ITEMS
+            if os.path.isdir(".venv"):
+                ACCESSABLE_MENU_ITEMS = MENU_ITEMS
+            continue
+
+if __name__ == "__main__":
+    main()
