cc37332a3fe7af05d197fa05245e779deaa6ac46
Anton Stechman <stechman.ab@gmail.com>
2026-03-10 22:57:53 +1300

    PATCH/4 patch pr build pkg version step (#5)
    # Pull Request Template
    
    ## Description
    
    Updates PR build process to auto-bump the package version, based off
    commit history on origin/main and the incooming PR title.
    ## Versioning
    
    Snowstream CLI uses **semantic versioning** in the format:
    
        [major].[minor].[patch]
    
    - **Major**: Incremented for breaking changes or major new features
    (e.g., `BREAKING-CHANGE:` PR tag).
    - **Minor**: Incremented for new features or enhancements (e.g.,
    `FEATURE:` PR tag).
    - **Patch**: Incremented for bug fixes, minor changes, or documentation
    updates (e.g., `PATCH:` PR tag).E:
    
    Fixes #4
    
    ## Type of Change
    
    Please delete options that are not relevant.
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update
    - [x] Refactor
    - [ ] Test coverage improvement
    
    ## Checklist
    
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [x] I have made corresponding changes to the documentation
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [x] New and existing unit tests pass locally with my changes
    - [x] Any dependent changes have been merged and published in downstream
    modules
    
    ## How Has This Been Tested?
    
    N/A
    
    - [ ] Test A
    - [ ] Test B
    
    ## Screenshots (if applicable)
    
    ## Related Issues
    
    ## Additional Notes
    
    Add any other context about the pull request here.
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ .github/workflows/scripts/update_version.py
@@ -0,0 +1,69 @@
+import os
+import subprocess
+import re
+
+# Get current version from pyproject.toml
+VERSION_FILE = os.path.join("src", "pyproject.toml")
+with open(VERSION_FILE, "r", encoding="utf-8") as f:
+    content = f.read()
+current_version = re.search(r'version = "(\d+)\.(\d+)\.(\d+)"', content)
+if current_version:
+    cv_major, cv_minor, cv_patch = map(int, current_version.groups())
+else:
+    cv_major, cv_minor, cv_patch = 0, 0, 0
+current_version: str = f"{cv_major}.{cv_minor}.{cv_patch}"
+major, minor, patch = 0, 0, 0
+
+# Get all commit messages in the branch
+result = subprocess.run(
+    ["git", "log", "origin/main", "--pretty=%B"]
+    , capture_output=True
+    , text=True
+)
+
+commit_messages = result.stdout.splitlines()
+
+# Append PR title from env var
+pr_title = os.environ.get("PR_TITLE", "")
+write_to_file: bool = os.environ.get("WRITE_VERSION", "true").upper() == "TRUE"
+
+if pr_title:
+    commit_messages.append(pr_title)
+
+for msg in commit_messages:
+    msg = msg.strip()
+    new_version = f"{major}.{minor}.{patch}"
+    if re.match(r"^BREAKING-CHANGE", msg, flags=re.IGNORECASE):
+        major += 1
+        minor = 0
+        patch = 0
+        print(f"Commit: {msg} (v{current_version} => v{new_version})")
+    elif re.match(r"^FEATURE", msg, flags=re.IGNORECASE):
+        minor += 1
+        patch = 0
+        print(f"Commit: {msg} (v{current_version} => v{new_version})")
+    elif re.match(r"^PATCH", msg, flags=re.IGNORECASE):
+        patch += 1
+        print(f"Commit: {msg} (v{current_version} => v{new_version})")
+
+new_version = f"{major}.{minor}.{patch}"
+if major < cv_major:
+    raise Exception(f"Incorrect Version Signature {new_version} < {current_version}")
+if major == cv_major and minor < cv_minor:
+    raise Exception(f"Incorrect Version Signature {new_version} < {current_version}")
+if major == cv_major and minor == cv_minor and patch < cv_patch:
+    raise Exception(f"Incorrect Version Signature {new_version} < {current_version}")
+
+version_summary = f"{current_version} => {new_version}"
+new_content = re.sub(r'version = ".*"', f'version = "{new_version}"', content)
+
+if write_to_file:
+    with open(VERSION_FILE, "w", encoding="utf-8") as f:
+        f.write(new_content)
+
+print(f"Updated version to {new_version} in {VERSION_FILE}")
+
+# write to GitHub env file
+with open(os.environ["GITHUB_OUTPUT"], "a") as f:
+    f.write(f"version_summary={version_summary}\n")
+    f.write(f"new_version={new_version}\n")
